eb2d31b1eeaf227b502f9275534190f6ab97be04
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-06-14 23:24:28 -0400

    Added fun message 📦

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ let secondaryContainer = document.getElementsByClassName('secondary-container');
 
 console.log(radioChoices);
 hideSecondaryContainer();
+window.alert('Just like that, we got it done 🥂')
+
+
 //Runs when the form is submitted
 form.onsubmit = function(e){
     e.preventDefault();
