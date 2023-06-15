02124c57c3dc40a610390025e4d254e3c1c3b369
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-06-14 23:18:44 -0400

    Completed program 📦

--- main.js
+++ main.js
@@ -3,17 +3,39 @@ let mainContainer = document.getElementsByClassName('main-container');
 let radioChoices = document.querySelectorAll('input[type="radio"]');
 let radioChoice; 
 let userAnswer = document.getElementById('user-answer');
+let secondaryContainer = document.getElementsByClassName('secondary-container');
 
 console.log(radioChoices);
+hideSecondaryContainer();
 //Runs when the form is submitted
 form.onsubmit = function(e){
     e.preventDefault();
     radioChoice = document.querySelector('input[type="radio"]:checked');
     console.log(radioChoice.value);
     changeNumber();
+    hideMainContainer();
+    showSecondaryContainer();
 }
 
 //Change the number in the secondary container
 function changeNumber (){
     userAnswer.innerHTML = radioChoice.value;
+}
+
+//Hide the main container
+function hideMainContainer(){
+    mainContainer[0].style.display = 'none';
+}
+//Show the main container
+function showMainContainer(){
+    mainContainer[0].style.display = 'block';
+}
+
+//Hide the secondary container
+function hideSecondaryContainer(){
+    secondaryContainer[0].style.display = 'none';
+}
+//Show the secondary container
+function showSecondaryContainer(){
+    secondaryContainer[0].style.display = 'block';
 }
